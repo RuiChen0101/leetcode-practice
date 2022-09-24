a0173a0328c807f6ddd50f410ec808e2f869ea71
SpiritCat0101 <gamain0101@gmail.com>
2022-09-24 12:59:17 +0800

    283, 344, 557, update 977

--- main.cpp
+++ main.cpp
@@ -1,28 +1,22 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
-void rotate(vector<int>& nums, int k) {
-    int len = nums.size();
-    k = k % len;
-    auto start = nums.begin();
-    auto mid = start + (len - k);
-    auto end = nums.end();
-
-    vector<int> result(len);
-
-    copy(mid, end, result.begin());
-    copy(start, mid, result.begin() + k);
-    nums = result;
+string reverseWords(string s) {
+    int i = 0;
+    for (int j = 0; j < s.length(); j++) {
+        if (s[j] == ' ') {
+            reverse(s.begin() + i, s.begin() + j);
+            i = j + 1;
+        }
+    }
+    reverse(s.begin() + i, s.end());
+    return s;
 }
 
 int main() {
-    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
-    rotate(nums, 11);
-    for (int n : nums) {
-        cout << n << " ";
-    }
-    cout << endl;
+    cout << reverseWords("Let's take LeetCode contest") << endl;
     return 0;
 }
