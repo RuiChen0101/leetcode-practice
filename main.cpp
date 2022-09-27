1da73dfb3a3a00d8ef9d7f87cad26b7c10cf611d
SpiritCat0101 <gamain0101@gmail.com>
2022-09-27 18:44:53 +0800

    3, 438, 567, 695, 733

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
 #include <vector>
 
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
-};
+using namespace std;
 
-ListNode* removeNthFromEnd(ListNode* head, int n) {
-    int count = 0;
-    ListNode* prev = head;
-    ListNode* current = head;
-    while (current->next != nullptr) {
-        current = current->next;
-        count++;
-        if (count > n) {
-            prev = prev->next;
+int islandFinding(vector<vector<int>>& grid, int m, int n, int i, int j) {
+    if (i < 0 || i >= m || j < 0 || j >= n) return 0;
+    if (grid[i][j] != 1) return 0;
+    grid[i][j] = 2;
+    int r1 = islandFinding(grid, m, n, i + 1, j);
+    int r2 = islandFinding(grid, m, n, i - 1, j);
+    int r3 = islandFinding(grid, m, n, i, j + 1);
+    int r4 = islandFinding(grid, m, n, i, j - 1);
+    return 1 + r1 + r2 + r3 + r4;
+}
+
+int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    int maxSize = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] != 1) continue;
+            maxSize = max(maxSize, islandFinding(grid, m, n, i, j));
         }
     }
-    if (count == (n - 1)) {
-        return head->next;
-    }
-    prev->next = prev->next->next;
-    return head;
+    return maxSize;
 }
 
 int main() { return 0; }
