33eae2616094dc10721c046c65ae72b777e6e4a9
SpiritCat0101 <gamain0101@gmail.com>
2022-10-01 13:47:26 +0800

    116, 206, 542, 617, 994

--- main.cpp
+++ main.cpp
@@ -1,31 +1,34 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
-int islandFinding(vector<vector<int>>& grid, int m, int n, int i, int j) {
-    if (i < 0 || i >= m || j < 0 || j >= n) return 0;
-    if (grid[i][j] != 1) return 0;
-    grid[i][j] = 2;
-    int r1 = islandFinding(grid, m, n, i + 1, j);
-    int r2 = islandFinding(grid, m, n, i - 1, j);
-    int r3 = islandFinding(grid, m, n, i, j + 1);
-    int r4 = islandFinding(grid, m, n, i, j - 1);
-    return 1 + r1 + r2 + r3 + r4;
-}
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
 
-int maxAreaOfIsland(vector<vector<int>>& grid) {
-    int m = grid.size();
-    int n = grid[0].size();
-    int maxSize = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (grid[i][j] != 1) continue;
-            maxSize = max(maxSize, islandFinding(grid, m, n, i, j));
-        }
+ListNode* reverseList(ListNode* head) {
+    ListNode* curr = head;
+    ListNode* prev = nullptr;
+    if (head == nullptr || head->next == nullptr) {
+        return head;
     }
-    return maxSize;
+    while (curr->next != nullptr) {
+        ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    curr->next = prev;
+    head = curr;
+
+    return head;
 }
 
 int main() { return 0; }
